9dab70a7c6d56faad29dd163186302ff81fd8c80
Fabien Costa <fabiencosta94800@gmail.com>
2024-08-29 13:37:58 +0200

    skills component

--- app/src/components/skills/Skills.jsx
+++ app/src/components/skills/Skills.jsx
@@ -1,28 +1,37 @@
 import "./skills.scss";
+import html5Logo from "../../assets/logos/html-5.svg";
+import cssLogo from "../../assets/logos/css-3.svg";
+import sassLogo from "../../assets/logos/sass.svg";
+import jsLogo from "../../assets/logos/javascript.svg";
+import reactLogo from "../../assets/logos/react.svg";
+import reduxLogo from "../../assets/logos/redux.svg";
+import gitHubLogo from "../../assets/logos/github.svg";
+import gitLogo from "../../assets/logos/git.svg";
+import npmLogo from "../../assets/logos/npm.svg";
+import swaggerLogo from "../../assets/logos/swagger.svg";
+import postmanLogo from "../../assets/logos/postman.svg";
+import figmaLogo from "../../assets/logos/figma.svg";
 
 const Skills = () => {
   return (
     <div className="skills">
       <h1 className="skills_title">Compétences</h1>
       <div className="skills_talent">
-        <div>
-          <h3>Front-end</h3>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Vue</li>
-          </ul>
+        <div className="skills_logos">
+          <img src={html5Logo} alt="html" className="skills_logo" />
+          <img src={cssLogo} alt="css" className="skills_logo" />
+          <img src={sassLogo} alt="sass" className="skills_logo" />
+          <img src={jsLogo} alt="javascript" className="skills_logo" />
+          <img src={reactLogo} alt="react" className="skills_logo" />
+          <img src={reduxLogo} alt="redux" className="skills_logo" />
         </div>
-        <div>
-          <h3>Back-end</h3>
-          <ul>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>MongoDB</li>
-            <li>MySQL</li>
-          </ul>
+        <div className="skills_logos">
+          <img src={gitHubLogo} alt="github" className="skills_logo" />
+          <img src={gitLogo} alt="git" className="skills_logo" />
+          <img src={swaggerLogo} alt="swagger" className="skills_logo" />
+          <img src={npmLogo} alt="npm" className="skills_logo" />
+          <img src={postmanLogo} alt="postman" className="skills_logo" />
+          <img src={figmaLogo} alt="figma" className="skills_logo" />
         </div>
       </div>
     </div>
--- app/src/pages/homepage/homepage.jsx
+++ app/src/pages/homepage/homepage.jsx
@@ -1,4 +1,3 @@
-import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 import About from "../../components/about/About";
 import Banner from "../../components/banner/Banner";
 import Contact from "../../components/contact/Contact";
@@ -8,21 +7,13 @@ import "./homepage.scss";
 
 const HomePage = () => {
   return (
-    <ParallaxProvider>
-      <div className="homepage">
-        <Parallax speed={-20}>
-          <Banner />
-        </Parallax>
-
-        <Parallax speed={10}>
-          <About />
-        </Parallax>
-
-        <Skills />
-        <Portfolio />
-        <Contact />
-      </div>
-    </ParallaxProvider>
+    <div className="homepage">
+      <Banner />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </div>
   );
 };
 
