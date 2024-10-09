2e117cc77eddd620de24041b6fd5d6c6d42c8fca
Fabien Costa <fabiencosta94800@gmail.com>
2024-10-09 15:09:31 +0200

    parallax effect

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import "./footer.scss";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>© 2024 - Fabien Costa / Portfolio en Open source</p>
+    <footer className="footer">
+      <p className="footer_text">© 2024 - Fabien Costa </p>
     </footer>
   );
 };
--- app/src/pages/homepage/homepage.jsx
+++ app/src/pages/homepage/homepage.jsx
@@ -5,26 +5,45 @@ import Contact from "../../components/contact/Contact";
 import Portfolio from "../../components/portfolio/Portfolio";
 import Skills from "../../components/skills/Skills";
 import "./homepage.scss";
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <section id="home">
-        <Banner />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="portfolio">
-        <Portfolio />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-    </div>
+    <ParallaxProvider>
+      <div className="homepage">
+        {/* Section Banner avec position fixe */}
+        <section id="home" className="banner-section">
+          <Banner />
+        </section>
+
+        {/* Les autres sections défilent par-dessus la bannière */}
+        <div className="content">
+          <Parallax speed={0}>
+            <section id="about">
+              <About />
+            </section>
+          </Parallax>
+
+          <Parallax speed={0}>
+            <section id="skills">
+              <Skills />
+            </section>
+          </Parallax>
+
+          <Parallax speed={0}>
+            <section id="portfolio">
+              <Portfolio />
+            </section>
+          </Parallax>
+
+          <Parallax speed={0}>
+            <section id="contact">
+              <Contact />
+            </section>
+          </Parallax>
+        </div>
+      </div>
+    </ParallaxProvider>
   );
 };
 
